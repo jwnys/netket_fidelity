b09846f455edf85d55b3320b2df813553c83646a
Jannes Nys <jannesnys@gmail.com>
2023-11-23 11:27:48 +0100

    Hopefully correct chunking

--- netket_fidelity/infidelity/overlap_U/expect.py
+++ netket_fidelity/infidelity/overlap_U/expect.py
@@ -6,23 +6,35 @@ from jax.scipy.special import logsumexp
 
 from netket import jax as nkjax
 from netket.operator import DiscreteJaxOperator
-from netket.vqs import MCState, expect, expect_and_grad, get_local_kernel_arguments
+from netket.vqs import MCState, expect, expect_and_grad, get_local_kernel_arguments, get_local_kernel
 from netket.utils import mpi
 
 from netket_fidelity.utils import expect_2distr
 
 from .operator import InfidelityOperatorUPsi
 
+def validate_arguments(vstate: MCState, op: InfidelityOperatorUPsi, chunk_size: int):
+    """ Function to check the restrictions we imposed in this file. """
+    if not isinstance(op._U, DiscreteJaxOperator):
+        raise ValueError("Only works with discrete operators.")
+    if not isinstance(op._U_dagger, DiscreteJaxOperator):
+        raise ValueError("Only works with discrete operators.")
+    
 
 @expect.dispatch
 def infidelity(vstate: MCState, op: InfidelityOperatorUPsi, chunk_size: int = None):
     if op.hilbert != vstate.hilbert:
         raise TypeError("Hilbert spaces should match")
 
+    validate_arguments(vstate, op, chunk_size)
+
+    # for DiscreteJaxOperator: args = operator itself!
+    
     sigma, args = get_local_kernel_arguments(vstate, op._U)
     sigma_t, args_t = get_local_kernel_arguments(op.target, op._U_dagger)
 
     return infidelity_sampling_MCState(
+        chunk_size,
         vstate._apply_fun,
         op.target._apply_fun,
         vstate.parameters,
@@ -35,7 +47,6 @@ def infidelity(vstate: MCState, op: InfidelityOperatorUPsi, chunk_size: int = No
         args_t,
         op.cv_coeff,
         return_grad=False,
-        chunk_size=chunk_size,
     )
 
 
@@ -50,10 +61,14 @@ def infidelity(  # noqa: F811
     if op.hilbert != vstate.hilbert:
         raise TypeError("Hilbert spaces should match")
 
+    validate_arguments(vstate, op, chunk_size)
+
     sigma, args = get_local_kernel_arguments(vstate, op._U)
     sigma_t, args_t = get_local_kernel_arguments(op.target, op._U_dagger)
 
+
     return infidelity_sampling_MCState(
+        chunk_size,
         vstate._apply_fun,
         op.target._apply_fun,
         vstate.parameters,
@@ -66,16 +81,17 @@ def infidelity(  # noqa: F811
         args_t,
         op.cv_coeff,
         return_grad=True,
-        chunk_size=chunk_size,
     )
 
 def process_local_arguments(args, σ, n_samples):
     """ args = operator (jax), or xp and mels ()"""
     N = σ.shape[-1]
     if isinstance(args, DiscreteJaxOperator):
-        xp, mels = args.get_conn_padded(σ)
-        n_xp = args.max_conn_size
+        op = args
+        xp, mels = op.get_conn_padded(σ)
+        n_xp = op.max_conn_size
     else:
+        raise Exception("no longer supported, since it messes up the chunking")
         xp = args[0].reshape(σ.shape[0], -1, N)
         mels = args[1].reshape(σ.shape[0], -1)
         n_xp = args[0].shape[-2]
@@ -84,17 +100,22 @@ def process_local_arguments(args, σ, n_samples):
     return xp_ravel, mels, n_xp
 
 def local_fidelity_kernel(
-        afun, params, σ, xp_ravel, mels, n_xp,
-        afun_t, params_t, σ_t, xp_t_ravel, mels_t, n_xp_t, 
+        afun, params, σ, args,
+        afun_t, params_t, σ_t, args_t, 
         cv_coeff=None, 
         model_state=None, model_state_t=None
     ):
     """ Part handling the local fidelity kernel. """
+    
     model_state = model_state or {}
     model_state_t = model_state_t or {}
     
     W = {"params": params, **model_state}
     W_t = {"params": params_t, **model_state_t}
+
+    n_samples = σ.shape[0]
+    xp_ravel, mels, n_xp = process_local_arguments(args, σ, n_samples)
+    xp_t_ravel, mels_t, n_xp_t = process_local_arguments(args_t, σ_t, n_samples)
 
     logpsi_t_xp = jnp.split(afun_t(W_t, xp_ravel), n_xp, axis=0)
     logpsi_t_xp = jnp.stack(logpsi_t_xp, axis=1)
@@ -114,8 +135,9 @@ def local_fidelity_kernel(
     return res
 
 
-@partial(jax.jit, static_argnames=("afun", "afun_t", "return_grad", "chunk_size"))
+@partial(jax.jit, static_argnames=("afun", "afun_t", "return_grad", "chunk_size",))
 def infidelity_sampling_MCState(
+    chunk_size,
     afun,
     afun_t,
     params,
@@ -128,24 +150,20 @@ def infidelity_sampling_MCState(
     args_t,
     cv_coeff,
     return_grad,
-    chunk_size,
 ):
     N = sigma.shape[-1]
     n_chains_t = sigma_t.shape[-2]
 
     σ = sigma.reshape(-1, N)
     σ_t = sigma_t.reshape(-1, N)
-    n_samples = σ.shape[0]
 
-    xp_ravel, mels, n_xp = process_local_arguments(args, σ, n_samples)
-    xp_t_ravel, mels_t, n_xp_t = process_local_arguments(args_t, σ_t, n_samples)
 
     def expect_kernel(params):
 
         def kernel_fun(params, params_t, σ, σ_t):
             return local_fidelity_kernel(
-                afun, params, σ, xp_ravel, mels, n_xp,
-                afun_t, params_t, σ_t, xp_t_ravel, mels_t, n_xp_t,
+                afun, params, σ, args,
+                afun_t, params_t, σ_t, args_t,
                 cv_coeff=cv_coeff, 
                 model_state=model_state, 
                 model_state_t=model_state_t
